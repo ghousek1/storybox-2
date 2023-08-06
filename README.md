f5cfe30bb258ab8ad101fdd81914b099af564d4c
ghousek1 <ghousek1@outlook.com>
2023-08-06 21:58:42 +0530

    chat app

--- README.md
+++
@@ -1,1 +0,0 @@
-# storybox-2
--- entities/User.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-
-const UserSchema  = new mongoose.Schema({
-    email: String,
-    password: String
-})
-
-export const UserModel = mongoose.model("User",UserSchema);
